3d1b26e1c1bfd1044ffb0f358ab5eda00cd7b5d5
miyamiko <w19718heart@gmail.com>
2025-05-14 23:52:54 +0800

    优化

--- Services/SYS/IT_SYS_UserContextService.cs
+++ Services/SYS/IT_SYS_UserContextService.cs
@@ -14,19 +14,40 @@ namespace MyFirstApi.Services
         /// 获取当前登录用户姓名
         /// </summary>
         string GetCurrentEmpName();
-
+        /// <summary>
+        /// 获取当前登录用户所属组织编码
+        /// </summary>
+        /// <returns></returns>
         string GetCurrentOrgCode();
-
+        /// <summary>
+        /// 获取当前登录用户所属组织名称
+        /// </summary>
+        /// <returns></returns>
         string GetCurrentOrgName();
-
+        /// <summary>
+        /// 获取当前登录用户所属岗位编码
+        /// </summary>
+        /// <returns></returns>
         string GetCurrentPostCode();
-
-        string GetCurrentPostName();    
-
+        /// <summary>
+        /// 获取当前登录用户所属岗位名称
+        /// </summary>
+        /// <returns></returns>
+        string GetCurrentPostName();
+        /// <summary>
+        /// 获取当前登录用户性别
+        /// </summary>
+        /// <returns></returns>
         string GetCurrentSex();
-
+        /// <summary>
+        /// 获取当前登录用户生日
+        /// </summary>
+        /// <returns></returns>
         string GetCurrentBirthday();
-
+        /// <summary>
+        /// 获取当前登录用户手机号码
+        /// </summary>
+        /// <returns></returns>
         string GetCurrentPhoneNumber(); 
         /// <summary>
         /// 同时获取当前登录用户的工号和姓名
